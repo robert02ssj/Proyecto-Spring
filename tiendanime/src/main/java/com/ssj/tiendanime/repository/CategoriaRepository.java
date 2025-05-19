12b9219bf1de61c134b8d1e8a226bc2f2e898a8a
Robert <byrobertofd7@gmail.com>
2025-05-19 10:45:19 +0200

    Corrijo CatRepository

--- tiendanime/src/main/java/com/ssj/tiendanime/repository/CategoriaRepository.java
+++ tiendanime/src/main/java/com/ssj/tiendanime/repository/CategoriaRepository.java
@@ -2,13 +2,10 @@ package com.ssj.tiendanime.repository;
 
 import com.ssj.tiendanime.model.Categoria;
 
-import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-    @Override
-    default Optional<Categoria> findById(Integer id) {
-        return Optional.empty();
-    }
+    // Inherit findById from JpaRepository without overriding
 }
